819ccbf8b7639432dfc21604416332631e0d3e8f
chandler05 <66492208+chandler05@users.noreply.github.com>
2021-06-25 12:29:44 -0500

    Initial commit

---
+++ src/main/java/com/chandler/dangers/Dangers.java
@@ -0,0 +1,24 @@
+package com.chandler.dangers;
+
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+@Mod("chandlers_dangers")
+public class Dangers
+{
+    private static final Logger LOGGER = LogManager.getLogger();
+
+    public Dangers() {
+        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
+
+        MinecraftForge.EVENT_BUS.register(this);
+    }
+
+    private void setup(final FMLCommonSetupEvent event) {
+
+    }
+}
